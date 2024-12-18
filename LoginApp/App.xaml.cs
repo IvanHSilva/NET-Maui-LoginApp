e31fc181d489060eee85b33c29c1c78df87bd6f7
IvanHS <ivanhenriquessilva@gmail.com>
2024-12-18 12:01:53 -0300

    Window side and position

--- LoginApp/App.xaml.cs
+++ LoginApp/App.xaml.cs
@@ -1,15 +1,20 @@
-﻿namespace LoginApp
-{
-    public partial class App : Application
-    {
-        public App()
-        {
+﻿namespace LoginApp {
+    public partial class App : Application {
+        const int WindowWidth = 400;
+        const int WindowHeight = 750;
+        const int WindowXPos = 100;
+        const int WindowYPos = 100;
+
+        public App() {
             InitializeComponent();
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+        protected override Window CreateWindow(IActivationState activationState) =>
+            new(new AppShell()) {
+                Width = WindowWidth,
+                Height = WindowHeight,
+                X = WindowXPos,
+                Y = WindowYPos
+            };
     }
 }
