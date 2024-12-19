41911b00a0b1cc51c41e3b68528fdfc32b97209d
IvanHS <ivanhenriquessilva@gmail.com>
2024-12-19 18:58:31 -0300

    User class

---
+++ LoginApp/User.cs
@@ -0,0 +1,14 @@
+﻿namespace LoginApp; 
+
+public class User {
+
+    public string Name { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+
+    public User() {}
+
+    public User(string name, string password) {
+        Name = name;
+        Password = password;
+    }
+}
