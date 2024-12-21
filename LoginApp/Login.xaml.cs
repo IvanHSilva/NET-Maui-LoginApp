428c1f49d2a990a6bd4102e03892ca5a4576603e
IvanHS <ivanhenriquessilva@gmail.com>
2024-12-21 10:18:54 -0300

    Login test

--- LoginApp/Login.xaml.cs
+++ LoginApp/Login.xaml.cs
@@ -1,17 +1,33 @@
 namespace LoginApp;
 
-public partial class Login : ContentPage
-{
-	public Login()
-	{
-		InitializeComponent();
-	}
+public partial class Login : ContentPage {
+    public Login() {
+        InitializeComponent();
+    }
 
-    private void Button_Clicked(object sender, EventArgs e) {
-		try {
+    private async void Button_Clicked(object sender, EventArgs e) {
+        try {
 
-		} catch(Exception ex) {
-			DisplayAlert("Ops", ex.Message, "Ok");
-		}
+            List<User> users = [
+                new User("José Maria", "123456"),
+                new User("Maria José", "001002"),
+                new User("João Rolão", "789000"),
+            ];
+
+            User newUser = new(user_text.Text, pass_text.Text);
+
+            if(users.Any(u => newUser.Name == u.Name &&
+                newUser.Password == u.Password)) {
+                
+                await SecureStorage.Default.SetAsync("loggeduser", newUser.Name);
+                Application.Current!.Windows[0].Page = new Protected(); 
+            } else {
+                throw new Exception("Usuário ou Senha incorretos!");
+            }
+
+        }
+        catch (Exception ex) {
+            await DisplayAlert("Login inválido!", ex.Message, "Ok");
+        }
     }
 }
